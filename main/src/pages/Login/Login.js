e7fa0a0aa513c6221e6ac7eb0600f9af41796496
kian.alavi <kian.alavi@partsoftware.com>
2023-07-13 22:51:40 +0430

    add className for input and button

--- main/src/pages/Login/Login.js
+++ main/src/pages/Login/Login.js
@@ -1,18 +1,27 @@
 import React from "react";
 import Form from "../../component/Form/Form";
-import "./Login.css";
+import styles from "./Login.module.css";
 
 export default function Login() {
   return (
     <Form>
-      <input type="email" placeholder="Email" name="email" id="email" />
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        id="email"
+        className={styles.loginInput}
+      />
       <input
         type="password"
         placeholder="Password"
         name="password"
         id="password"
+        className={styles.loginInput}
       />
-      <button type="submit">Login</button>
+      <button type="submit" className={styles.login}>
+        Login
+      </button>
     </Form>
   );
 }
