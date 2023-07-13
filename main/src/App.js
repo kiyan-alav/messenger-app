8ffba13e31bf9c253b51ecaa0d752f2f004347bc
kian.alavi <kian.alavi@partsoftware.com>
2023-07-13 23:13:13 +0430

    add all pages to App.js for testing

--- main/src/App.js
+++ main/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Chat from "./pages/Chat/Chat";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
@@ -6,9 +7,10 @@ import Register from "./pages/Register/Register";
 function App() {
   return (
     <>
+      {/* <Home /> */}
       {/* <Register /> */}
       {/* <Login /> */}
-      {/* <Home /> */}
+      {/* <Chat /> */}
     </>
   );
 }
