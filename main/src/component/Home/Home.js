10b0bf60ab7c5af9a85cc55d63a5bd184bbf4118
kian.alavi <kian.alavi@partsoftware.com>
2023-07-13 10:37:57 +0430

    ChatLogo and ChatInto component added to Home component

--- main/src/component/Home/Home.js
+++ main/src/component/Home/Home.js
@@ -1,26 +1,26 @@
 import React from "react";
+import ChatLogo from "./../ChatLogo/ChatLogo";
+import ChatIntro from "./../ChatIntro/ChatIntro";
+
 import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <main id="home">
-      <div className={styles.chatLogo}>
-        <img src="./img/phoenix.svg" alt="logo" />
-      </div>
-      <div className={styles.chatIntro}>
-        <h2 className={styles.chatTitle}>Phoenix</h2>
-        <p>
+    <main className={styles.home}>
+      <ChatLogo />
+      <ChatIntro>
+        <p className={styles.chatIntroText}>
           Welcome To Phoenix Chat App. The First Messenger App That Developed By
           Kiyan Alavi :). Hope You Enjoy!!
         </p>
-      </div>
+      </ChatIntro>
       <div className={styles.registerLogin}>
         <p>
-          Didn't you have account?
+          Didn't you have account?{" "}
           <a href="./register.html" className="create">
             Create one
           </a>{" "}
-          or
+          or{" "}
           <a href="./login.html" className="login">
             Login
           </a>
