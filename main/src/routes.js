3331fab888723398a09d4f68d8ba15203e3760b1
Kiyan Alavi <81843676+kiyan-alav@users.noreply.github.com>
2023-07-21 10:03:55 +0430

    routes created

---
+++ main/src/routes.js
@@ -0,0 +1,11 @@
+import Chat from "./pages/Chat/Chat";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+
+export const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "/chat/:userId", element: <Chat /> },
+];
