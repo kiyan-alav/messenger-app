c77491a86e1febc3524f39aa6b414e300fb9390d
Kiyan Alavi <81843676+kiyan-alav@users.noreply.github.com>
2023-07-31 11:37:58 +0430

    feat: search user logic add

--- main/src/component/UsersList/UsersList.js
+++ main/src/component/UsersList/UsersList.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import supabase from "./../../config/supabaseClient";
 import styles from "./UsersList.module.css";
 import User from "../User/User";
 
 export default function UsersList() {
+  const [userName, setUserName] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async function () {
+    const { data, error } = await supabase
+      .from("users")
+      .select("fullName")
+      .eq(userName);
+
+    if (data) {
+      console.log(data);
+    } else {
+      console.log(error);
+    }
+  };
+
+  const handleKey = function (e) {
+    console.log(e.code);
+    if (e.code === "ENTER") {
+      e.preventDefault();
+      handleSearch();
+    }
+    // e.code === "ENTER" && handleSearch();
+  };
+
   return (
     <section className={styles.usersList}>
       <h2>Chats List</h2>
@@ -13,6 +40,9 @@ export default function UsersList() {
             name="search"
             id="search"
             placeholder="Search For Users..."
+            onChange={(e) => setUserName(e.target.value)}
+            onKeyDown={handleKey}
+            // value={userName}
           />
         </div>
         <div className={styles.searchIcon}>
