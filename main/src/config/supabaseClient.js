43494743cb7a7ed6f73841eb7a414100f6bc057b
Kiyan Alavi <81843676+kiyan-alav@users.noreply.github.com>
2023-11-30 11:00:10 +0330

    V0.1.7:
    - delete supabaseClient.js

--- main/src/config/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "YOUR_API-URL";
-const supabaseKey = "YOUR_API_KEY";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
